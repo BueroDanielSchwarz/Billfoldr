740720d38ae2c81d1b62793e3d4042bc18f59503
BueroDanielSchwarz <mail@daniel-schwarz.de>
2026-03-12 18:42:35 +0100

    Update stripe.ts

--- billfoldr-billing-vercel/src/lib/stripe.ts
+++ billfoldr-billing-vercel/src/lib/stripe.ts
@@ -12,9 +12,7 @@ if (!process.env.STRIPE_PRICE_OVERAGE_METERED) {
   throw new Error('STRIPE_PRICE_OVERAGE_METERED missing');
 }
 
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-  apiVersion: '2026-02-25.clover',
-});
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
 
 export const PRICE_YEARLY_FIXED = process.env.STRIPE_PRICE_YEARLY_FIXED;
 export const PRICE_OVERAGE_METERED = process.env.STRIPE_PRICE_OVERAGE_METERED;
